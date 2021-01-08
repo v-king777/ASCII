e41e9eab3632a2d0a8971c2eb641fcb289956b22
v-kolp <v.kolpashchikov@gmail.com>
2021-01-08 23:06:29 +0300

    Добавьте файлы проекта.

---
+++ ASCII/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа выводит все символы ASCII с десятичными кодами
+using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = new char[96];
+            int code = 32;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)code;
+                Console.WriteLine("char {0} code {1}", chars[i], code++);
+            }
+            Console.WriteLine("\nДля продолжения нажмите любую клавишу . . .");
+            Console.ReadKey();
+        }
+    }
+}
